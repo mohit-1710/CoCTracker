1ed6b59d1c9dcb2e103bf329202e3537d37d7dc0
mohit-1710 <megamohit2006.com>
2025-04-22 19:41:46 +0530

    modification

--- utils/Footer.js
+++ utils/Footer.js
@@ -1,12 +1,11 @@
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
 import { AiFillGithub } from "react-icons/ai";
-import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <div className="flex flex-row w-full justify-center p-6 gap-2">
-        <Link href="https://github.com/milinddhamu/coc-nextjs">
+        <Link href="https://github.com/mohit-1710/CoCTracker">
             <Button
               id="github"
               iconOnly
@@ -16,17 +15,6 @@ const Footer = () => {
               icon={<AiFillGithub className="scale-125"/>}
             />
           </Link>
-          <Link href="https://github.com/milinddhamu/coc-nextjs">  
-            <Button
-              id="twitter"
-              auto
-              bordered
-              css={{borderWidth:"0.5px",borderColor:"#6b728050"}}
-              color="neutral"
-              icon={<FaXTwitter className="scale-125"/>} >
-                @milind_dhamu
-            </Button>
-            </Link>
       </div>
   );
 }
